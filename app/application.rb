78509bc7064acff91a4203d32d355877b3010cbc
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-07-28 11:54:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     
     time = Time.now.hour
-    if time < 
+    if time < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
